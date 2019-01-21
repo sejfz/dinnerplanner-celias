f1b0450516f10dccde5c30563fbf15e4f092ea2d
Celina Zander <zander.celina@gmail.com>
2019-01-21 13:57:22 +0100

    Update homeView.js

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -17,7 +17,7 @@ var homeView = function (container, model) {
 	 * Here we use @var {jQuery object} numberOfGuests that is a reference to <span>
 	 * in our view to dynamically set it's value to "Hello World".
 	 */
-	numberOfGuests.html("getGuest");
+	numberOfGuests.html();
 	
 }
 
