54bac904ff8bd62c34308244e35b05c12e7df4d8
Celina Zander <zander.celina@gmail.com>
2019-01-21 13:32:52 +0100

    Update homeView.js

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -1,1 +1,23 @@
+var homeView = function (container, model) {
+	
+	/**
+	 * IMPORTANT: Never use $('someSelector') directly in the views. Always use container.find
+	 * or some other way of searching only among the containers child elements. In this way you
+	 * make your view code modular and ensure it dosn't break if by mistake somebody else
+	 * in some other view gives the same ID to another element.
+	 * 
+	 */
+  
+	var numberOfGuests = container.find("#numberOfGuests");
+
+	this.plusButton = container.find("#plusGuest");
+	this.minusButton = container.find("#minusGuest");
+	
+	/**
+	 * Here we use @var {jQuery object} numberOfGuests that is a reference to <span>
+	 * in our view to dynamically set it's value to "Hello World".
+	 */
+	numberOfGuests.html("0");
+	
+}
 
