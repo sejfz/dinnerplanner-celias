20fe5ff0214f89c0c20301911e1321e12ed238fb
Celina Zander <zander.celina@gmail.com>
2019-01-25 10:09:04 +0100

    quick fix

--- js/app.js
+++ js/app.js
@@ -5,13 +5,6 @@ $(function() {
     console.log(container);
     var IngredientView = new ingredView(container, model);
 	
-	// And create the instance of ExampleView
-	// var exampleView = new ExampleView($("#exampleView"));
-	/**
-	 * IMPORTANT: app.js is the only place where you are allowed to
-	 * use the $('someSelector') to search for elements in the whole HTML.
-	 * In other places you should limit the search only to the children 
-	 * of the specific view you're working with (see exampleView.js).
-	 */
+    // ADD BOTH FIND AND DISH VIEW HERE
 
 });
