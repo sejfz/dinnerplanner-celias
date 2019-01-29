a8cfe72bf66a1aea7b7a44c04353c66ac2777da7
Celina Zander <zander.celina@gmail.com>
2019-01-29 13:54:28 +0100

    dinner view

--- js/app.js
+++ js/app.js
@@ -4,9 +4,10 @@ $(function() {
     var container = $("#ingredView")
     var IngredientView = new ingredView(container, model);
     
-   var container2 = $("#dishView")
+    var container2 = $("#dishView")
     var fullMenu = new dishView(container2, model);
 	//We instantiate our model
 
-
+    var container3 = $("#displayView")
+    var DisplayView = new displayView(container3, model);
 });
