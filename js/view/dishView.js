298f4d2efda423177c1a23eb96ec1bcce69c54e2
sejfz <eliashh97@hotmail.com>
2019-01-29 15:24:16 +0100

    såspåse

--- js/view/dishView.js
+++ js/view/dishView.js
@@ -1,14 +1,40 @@
 
 var dishView = function (card, model) {
 	
-    var allName = card.find("#dish");
-    var fullmenu = model.getFullMenu();
+    var allName = card.find("#dishView");
     var x;
-    var stri = "";
-    for (x in fullmenu) {
-        stri += "<img class=\"card-img-top\" src=\"images/" + fullmenu[x].image + "\" alt=\"" + fullmenu[x].image + "\">" + fullmenu[x].name + "<br/>";
+    var divList = [];
+    for (x in dishes){
+        var elem = document.createElement("div");
+        
+        var divclass = document.createAttribute("class");        
+        console.log(elem)
+        
+        var img = document.createElement("img");
+        elem.appendChild(img);
+        
+        var classs = document.createAttribute("class");
+        var src = document.createAttribute("src");
+        classs.value = "card-img-top";
+        src.value = "images/" + dishes[x].image;
+        
+        img.setAttribute("class", classs.value);
+        img.setAttribute("src", src.value);
+        
+        divList.push(elem);
+        console.log(divList)
     }
 
-    allName.html(stri);
+    /*var fullmenu = model.getFullMenu();
+    var stri = "";
+    var str = "";
+    for (x in fullmenu) {
+        stri += "<img class=\"card-img-top\" src=\"images/" + fullmenu[x].image + "\" alt=\"" + fullmenu[x].name + "\">" + fullmenu[x].name + </br>;
+        
+        str = ;
+        
+        
+    }*/
+    allName.html(divList);
 }
  
