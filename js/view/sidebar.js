bae60deed887e3fb886f877d4f87f948abd951bc
sejfz <eliashh97@hotmail.com>
2019-02-02 17:17:04 +0100

    yup sidebar är typ klar

--- js/view/sidebar.js
+++ js/view/sidebar.js
@@ -2,59 +2,70 @@ var sidebar = function (card, model) {
 	
     var sidB = card.find("#sidb");
     var topdiv = document.createElement("nav");
-    var button = document.createElement("button");
-    var div2 = document.createElement("div");
-    var myDinner = document.createElement("h4");
-    var dishName = document.createElement("p");
-    var cost = document.createElement("p");
-
     var topclass = document.createAttribute("class");
+    topclass.value = "navbar navbar-expand-sm navbar-light bg-light";
+    topdiv.setAttribute("class", topclass.value);
+
+    var button = document.createElement("button");
     var butType = document.createAttribute("type");
-    var div2class = document.createAttribute("class");
     var butClass = document.createAttribute("class");
     var butDataToggle = document.createAttribute("data-toggle");
     var butDataTarget = document.createAttribute("data-target");
-    var div2Id = document.createAttribute("id");
-    var div2Id2 = document.createAttribute("id");
-    var onresize = document.createAttribute("onresize");
-    var aria = document.createAttribute("aria-expanded");
-    var ariac = document.createAttribute("aria-controls");
-
-    var MD = document.createTextNode("My Dinner");
-    var DN = document.createTextNode("Dish Name: ");
-    var totcost = document.createTextNode("66 kr");
-    var butInfo = document.createTextNode("expand/collapse");
-
-    topclass.value = "navbar navbar-expand-sm navbar-light bg-light";
-    aria.value = "true";
-    div2class.value = "collapse";
+    var ariaExpanded = document.createAttribute("aria-expanded");
+    var ariaControls = document.createAttribute("aria-controls");
+    var ariaLabel = document.createAttribute("aria-label");
     butType.value = "button";
     butClass.value = "navbar-toggler";
     butDataToggle.value = "collapse";
     butDataTarget.value = "#navbarSupportedContent";
-    div2Id.value = "content";
-    ariac.value = "navbarSupportedContent";
-
-    button.setAttribute("aria-controls");
-    topdiv.setAttribute("class", topclass.value);
-    div2.setAttribute("aria-expanded", aria.value);
+    ariaExpanded.value = "false";
+    ariaControls.value = "navbarSupportedContent";
+    ariaLabel.value = "Toggle navigation";
     button.setAttribute("type", butType.value);
     button.setAttribute("class", butClass.value);
     button.setAttribute("data-toggle", butDataToggle.value);
     button.setAttribute("data-target", butDataTarget.value);
+    button.setAttribute("aria-expanded", ariaExpanded.value);
+    button.setAttribute("aria-controls", ariaControls.value);
+    button.setAttribute("aria-label", ariaLabel.value);
+    topdiv.appendChild(button);
+    
+    var span = document.createElement("span");
+    var spanClass = document.createAttribute("class");
+    spanClass.value = "navbar-toggler-icon";
+    span.setAttribute("class", spanClass.value);
+    button.appendChild(span);
+    
+    var div2 = document.createElement("div");
+    var div2class = document.createAttribute("class");
+    var div2Id = document.createAttribute("id");
+    div2class.value = "collapse navbar-collapse";
+    div2Id.value = "navbarSupportedContent";
     div2.setAttribute("class", div2class.value);
     div2.setAttribute("id", div2Id.value);
-
-    
-    topdiv.appendChild(button);
     topdiv.appendChild(div2);
-    button.appendChild(butInfo);
+    
+    var innerdiv = document.createElement("div");
+    var innerDivClass = document.createAttribute("class");
+    innerDivClass.value = "nav navbar-nav collapse navbar-collapse";
+    innerdiv.setAttribute("class", innerDivClass.value);
+    div2.appendChild(innerdiv);
+    
+    var myDinner = document.createElement("h4");
+    var MD = document.createTextNode("My Dinner");
     myDinner.appendChild(MD);
+    innerdiv.appendChild(myDinner);        
+
+    var dishName = document.createElement("p");
+    var DN = document.createTextNode("Dish Name: ");
     dishName.appendChild(DN);
+    innerdiv.appendChild(dishName);
+
+    var cost = document.createElement("p");
+    var totcost = document.createTextNode("66 kr");
     cost.appendChild(totcost);
-    div2.appendChild(myDinner);        
-    div2.appendChild(dishName);
-    div2.appendChild(cost);
+    innerdiv.appendChild(cost);
+
     
   
     
