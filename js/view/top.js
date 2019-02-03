081d624f1fd0858d45fa96d4dd3df26f26206201
Celina Zander <zander.celina@gmail.com>
2019-02-03 16:54:06 +0100

    attempt header

---
+++ js/view/top.js
@@ -0,0 +1,28 @@
+
+var top = function (card, model) {
+	
+    var niceHeader = card.find("#header");
+    
+        var topdiv = document.createElement("div");
+        var text = document.createElement("h1");
+        var place = document.createElement("p");
+    
+        var topclass = document.createAttribute("class");
+        var stylish = document.createAttribute("style");
+        var class2 = document.createAttribute("class");
+
+        topclass.value = "container-fluid";
+        stylish.value = "background-colour: antiquewhite";
+        class2.value = "text-center";
+    
+        topdiv.setAttribute("class", topclass.value);
+        text.setAttribute("style", stylish.value);
+        place.setAttribute("class", class2.value);
+                
+        divList.push(topdiv);
+        console.log(topdiv)
+    }
+    
+    niceHeader.html(HALP);
+ 
+
