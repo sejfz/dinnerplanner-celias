2faa7f905dceecde0964085dc03bf461c6a61622
sejfz <eliashh97@hotmail.com>
2019-02-04 11:15:52 +0100

    only goodviews

--- js/view/bannerView.js
+++
@@ -1,8 +0,0 @@
-var bannerView = function (container, model) {
-    var getMoney = container.find("#numpeep");
-    
-    var str = "";
-    str =   model.getNumberOfGuests();
-    
-    getMoney.html(str);
-}
--- js/view/sidebarView.js
+++
@@ -1,9 +0,0 @@
-
-var sidebarView = function (container, model) {
-    var getMoney = container.find("#money");
-    
-    var str = "";
-    str =   model.getTotalMenuPrice();
-    
-    getMoney.html(str);
-}
